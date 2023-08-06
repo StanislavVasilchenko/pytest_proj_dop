a89406c5014fe5a9af8bb9846890f5e90951ed4d
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-06 18:24:31 +0300

    test with parametrize

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -2,14 +2,13 @@ import pytest
 
 from utils.dict import get_val
 
-
-@pytest.fixture
-def collection():
-    return {"vcs": "mercurial"}
+dict_ = {"vcs": "mercurial"}
 
 
-def test_get_val(collection):
-    assert get_val(collection, "vcs") == "mercurial"
-    assert get_val(collection, "vcs", "git") == "mercurial"
-    assert get_val({}, "vcs", "git") == "git"
-    assert get_val({}, "vcs", "bazaar") == "bazaar"
+@pytest.mark.parametrize("collection, key, default, result", [(dict_, "vcs", "git", "mercurial"),
+                                                              ({}, "vcs", "git", "git"),
+                                                              ({}, "vcs", "bazaar", "bazaar"),
+                                                              (dict_, "asd", "git", "git")])
+def test_get_val(collection, key, default, result):
+    assert get_val(collection, key, default) == result
+
